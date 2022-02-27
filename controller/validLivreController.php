8c715a5d43b8a5fb2b25d7ce0e385ce4381585e9
glebrech <yoel.lebrech@gmail.com>
2022-02-27 15:17:22 +0100

    push

--- controller/validLivreController.php
+++ controller/validLivreController.php
@@ -1,18 +1,47 @@
 <?php
-class validLivreController  {
+class validLivreController
+{
 
     public function __construct()
-	{    
+    {
         session_start();
         error_reporting(0);
-      
-	  //TODO
-      if($operation=="insert"){
-}
-else {
-//erreur on renvoit à la page d'accueil
-header('Location: accueil.php?id='.$_SESSION["token"]);
-
-}
+        require_once "metier/Livre.php";
+        require_once "Constantes.php";
+        require_once "PDO/LivreDB.php";
+        require_once "PDO/connectionPDO.php";
+        require_once "controller/Controller.php";
+        //TODO
+        $auteur = $_POST["auteur"] ?? null;
+        $information = $_POST["info"] ?? null;
+        $edition = $_POST["edition"] ?? null;
+        $titre = $_POST["titre"] ?? null;
+        $operation = $_GET["operation"] ?? null;
+        if (Controller::auth())
+            if ($operation == "insert") {
+                try {
+                    $livreDB = new LivreDB($pdo);
+                    $res = $livreDB->ajout(new Livre($titre, $edition, $information, $auteur));
+                    if ($res) { ?>
+                    <div class="alert alert-sucess d-flex" role="alert">
+                        <svg class="bi flex-shrink-0 me-2" width="40" height="40" role="img" aria-label="Success:" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                        </svg>
+                        <div>
+                            <h4 class="alert-heading">Livre est bien ajouté dans la BDD</h4>
+                            Titre : <?php echo $titre; ?><br>
+                            Edition : <?php echo $edition; ?><br>
+                            Information : <?php echo $information; ?><br>
+                            Auteur : <?php echo $auteur; ?>
+                        </div>
+                    </div>
+<?php };
+                } catch (Exception $e) {
+                    throw new Exception(Constantes::EXCEPTION_INSERT_DB_LIVRE);
+                }
+            } else {
+                //erreur on renvoit à la page d'accueil
+                header('Location: accueil.php?id=' . $_SESSION["token"]);
+            }
     }
 }
--- tests/LivreDBTest.php
+++ tests/LivreDBTest.php
@@ -1,11 +1,14 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
+
 require_once "Constantes.php";
 require_once "metier/Livre.php";
 require_once "PDO/LivreDB.php";
 
 
-class LivreDBTest extends TestCase {
+class LivreDBTest extends TestCase
+{
 
     /**
      * @var LivreDB
@@ -17,7 +20,8 @@ class LivreDBTest extends TestCase {
      * Sets up the fixture, for example, opens a network connection.
      * This method is called before a test is executed.
      */
-    protected function setUp():void {
+    protected function setUp(): void
+    {
         //parametre de connexion à la bae de donnée
         $strConnection = Constantes::TYPE . ':host=' . Constantes::HOST . ';dbname=' . Constantes::BASE;
         $arrExtraParam = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
@@ -29,17 +33,26 @@ class LivreDBTest extends TestCase {
      * Tears down the fixture, for example, closes a network connection.
      * This method is called after a test is executed.
      */
-    protected function tearDown():void {
-        
+    protected function tearDown(): void
+    {
     }
 
     /**
      * @covers LivreDB::ajout
      * @todo   Implement testAjout().
      */
-    public function testAjout() {
+    public function testAjout()
+    {
         try {
-        //TODO
+            $this->livre = new LivreDB($this->pdodb);
+            $l = new Livre("Le Seigneur des anneaux", "livre de Tolkien", "TOme 1", "JRR Tolkien");
+            $this->livre->ajout($l);
+            $livre = $this->livre->selectLivre($l->getId());
+            $this->assertEquals($l->getAuteur(), $livre->getAuteur());
+            $this->assertEquals($l->getInformation(), $livre->getInformation());
+            $this->assertEquals($l->getEdition(), $livre->getEdition());
+            $this->assertEquals($l->getTitre(), $livre->getTitre());
+            $this->livre->suppression($livre);
         } catch (Exception $e) {
             echo 'Exception recue : ', $e->getMessage(), "\n";
         }
@@ -49,36 +62,80 @@ class LivreDBTest extends TestCase {
      * @covers LivreDB::update
      * @todo   Implement testUpdate().
      */
-    public function testUpdate() {
+    public function testUpdate()
+    {
         $this->object = new LivreDB($this->pdodb);
         $l = new Livre("Flaubert", "livre de Flaubert", "Galimard", "titre update");
         $l->setId(58);
-      $this->object->update($l);
-       //TODO  à finaliser 
+        $livre = $this->livre->selectLivre($l->getId());
+        $this->assertEquals($l->getAuteur(), $livre->getAuteur());
+        $this->assertEquals($l->getInformation(), $livre->getInformation());
+        $this->assertEquals($l->getEdition(), $livre->getEdition());
+        $this->assertEquals($l->getTitre(), $livre->getTitre());
+        $this->livre->suppression($livre);
+        $this->object->update($l);
     }
 
     /**
      * @covers LivreDB::suppression
      * @todo   Implement testSuppression().
      */
-    public function testSuppression() {
-       //TODO
+    public function testSuppression()
+    {
+        try {
+            $this->livre = new LivreDB($this->pdodb);
+            $this->livre->ajout(new Livre("Le Seigneur des anneaux", "livre de Tolkien", "TOme 1", "JRR Tolkien"));
+            $livre = $this->livre->selectLivre($this->pdodb->lastInsertId());
+            $this->livre->suppression($livre->getId());
+            $livre = $this->livre->selectLivre($livre->getId());
+            if ($livre != null) {
+                $this->markTestIncomplete(
+                    "La suppression du livre a échoué"
+                );
+            }
+        } catch (Exception $e) {
+            $exception = "Le livre n'est pas présent dans la base de donnée";
+            $this->assertEquals($exception, $e->getMessage());
+        }
     }
 
     /**
      * @covers LivreDB::selectAll
      * @todo   Implement testSelectAll().
      */
-    public function testSelectAll() {
-       //TODO
+    public function testSelectAll()
+    {
+        $this->livre = new LivreDB($this->pdodb);
+        $res = $this->livre->selectAll();
+        $i = 0;
+        $OK = true;
+        foreach ($res as $key => $value) {
+            $i++;
+        }
+        if ($i == 0) {
+            $this->markTestIncomplete(
+                'Aucun résultats'
+            );
+            $OK = false;
+        }
+        $this->assertTrue($OK);
     }
 
     /**
      * @covers LivreDB::selectLivre
      * @todo   Implement testSelectLivre().
      */
-    public function testSelectLivre() {
-     //TODO
+    public function testSelectLivre()
+    {
+        $this->livre = new LivreDB($this->pdodb);
+        $livres = new Livre("Le Seigneur des anneaux", "livre de Tolkien", "TOme 1", "JRR Tolkien");
+        $this->livre->ajout($livres);
+        $livres->setId($this->pdodb->lastInsertId());
+        $l = $this->livre->selectLivre($livres->getId());
+        $this->assertEquals($l->getId(), $livres->getId());
+        $this->assertEquals($l->getAuteur(), $livres->getAuteur());
+        $this->assertEquals($l->getInformation(), $livres->getInformation());
+        $this->assertEquals($l->getEdition(), $livres->getEdition());
+        $this->assertEquals($l->getTitre(), $livres->getTitre());
     }
-
 }
